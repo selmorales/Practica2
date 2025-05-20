a8ed1c224e8fef21604ed211a732927ada10594d
selmorales <selenamorales.p@gmail.com>
2025-05-20 17:36:59 +0200

    20 mayo

--- src/parte1/BusquedaBinaria.java
+++ src/parte1/BusquedaBinaria.java
@@ -6,7 +6,33 @@ public class BusquedaBinaria {
 	   caso peor array[b]
 	   caso promedio array[random(n)]
 	   */
+	public int[] getCoordinates(int i, int ancho) {//matrices CUADRADAS
+		//int fila = (int) Math.floor((float) i/ancho);
+		int fila = i/ancho; // división entera
+		int columna = i % ancho; //resto de la división
+		int[] coordenadas = {fila, columna};
+		return coordenadas;
+	}
 	
+
+	public static int[] binaryMatSearch(int[][] matrix, int target) {
+		// 1. Calcular el número de filas
+		// 2. Calcular el número de columnas
+		// 3. Establecer el índice inicial como 0
+		// 4. Establecer el último índice como la posición del último elemento
+		// 5. Mientras el índice inicial sea menor o igual que el último índice
+		// 5.1 Calcular el índice para la posición media, como un entero
+		// 5.2 Traducir este índice a coordenadas 2D (usando la función)
+		// 5.3 Obtener el valor de la matriz en la posición media
+		// 5.4 Si el valor en la posición media es igual al objetivo->
+		// 5.5 Devolver las coordenadas 2D donde se encuentra
+		// 5.6 En caso contrario: Si el valor en la posición media es menor que el objetivo->
+		// 5.7 Establecer el índice inicial como el índice medio + 1
+		// 5.8 En caso contrario (el valor en la posición media es mayor que el objetivo)->
+		// 5.9 Establecer el último índice como medio - 1
+		// 6. Ejecutar este código significa que el objetivo no está en la matriz, devuelve [-1,-1]
+
+	}
 	
 	
 	public static void main(String[] args) {
--- src/parte1/BusquedaLineal.java
+++ src/parte1/BusquedaLineal.java
@@ -17,6 +17,8 @@ public class BusquedaLineal {
 		}
 	
 	public static int[] linearMatSearch(int[][] mat, int valor) {
+		int m = mat.length;
+		int n = mat[0].length;
 		int counter = 0;
 		int[] result = { -1, -1, counter };
 		int x = mat[0].length; // filas
@@ -26,7 +28,7 @@ public class BusquedaLineal {
 		for(int i = 0; i < y; i++){ // Desde la fila 0 a la 2
 			//System.out.println("Fila: "+ i);
 			for(int j = 0; j < x; j++){//Desde columna 0 a 2 dentro de una fila i
-				//System.out.println("Columna: "+ i);
+				//System.out.println("Columna: "+ j);
 				counter++;
 				if (mat[i][j] == valor ) {
 					result[0] = i;
@@ -34,31 +36,45 @@ public class BusquedaLineal {
 				}
 			}
 		} result[2] = counter;
+		//%-15s alinea a la izq con 15 caracteres
+				System.out.printf("%-15s | %-5s | %-11s | %s\n", "Descripcion", "Talla",
+						"Iteraciones", "Resultado");
+				System.out.printf("%-15s | %-5d | %-11d | %s\n", "linearMatSearch",
+						m*n, result[2], "["+result[0]+","+result[1]+"]");
 		return result;
 		
 		}
 
 	
 	public static int[] successfulMatSearch(int[][] mat, int v) {
+		int m = mat.length;
+		int n = mat[0].length;
 		int counter = 0;
 		int[] result = { -1, -1, counter };
-		int x = mat[0].length; // filas
-		int y = mat.length; // columnas
+		int x = mat[0].length; // columnas
+		int y = mat.length; // filas
 		boolean encontrado = false;
-		while(encontrado == false) {
+	
 			for(int i = 0; i < y; i++){ // Desde la fila 0 a la 2
 				//System.out.println("Fila: "+ i);
 				for(int j = 0; j < x; j++){//Desde columna 0 a 2 dentro de una fila i
-					//System.out.println("Columna: "+ i);
+					//System.out.println("Columna: "+ j);
 					counter++;
 					if (mat[i][j] == v ) {
 						result[0] = i;
 						result[1] = j;
 						encontrado = true;
+						break;
 					}
 				}
+				if (encontrado == true) break;
 			} result[2] = counter;
-		}
+			//System.out.println(result[0]+ " " +result[1]+ " " +result[2]+ " " );
+		
+		System.out.printf("%-15s | %-5s | %-11s | %s\n", "Descripcion", "Talla",
+				"Iteraciones", "Resultado");
+		System.out.printf("%-15s | %-5d | %-11d | %s\n", "linearMatSearch",
+				m*n, result[2], "["+result[0]+","+result[1]+"]");
 		return result;
 		}
 
@@ -66,39 +82,69 @@ public class BusquedaLineal {
 	
 //////////////////////////////////////////////////////////////////////
 	public static void main(String[] args) {
-		int m = 7;
-		int n = 7;
+		int m = 5;
+		int n = 5;
 		
 		int[][] matrix = generateOrderedMatrix(m,n);
-		int elementoABuscar = matrix[4][3];
+		int elementoABuscar = matrix[1][1];
 		int[] resultado = linearMatSearch(matrix, elementoABuscar);
 		//Comprobaciones
 		System.out.println("Elemento buscado: " + elementoABuscar);
 		System.out.println("Elemento encontrado: " + matrix[resultado[0]][resultado[1]]);
 		System.out.println(resultado[0] + ", " + resultado[1]);
 		
-		
-		//%-15s alinea a la izq con 15 caracteres
-		System.out.printf("%-15s | %-5s | %-11s | %s\n", "Descripcion", "Talla",
-				"Iteraciones", "Resultado");
-		System.out.printf("%-15s | %-5d | %-11d | %s\n", "linearMatSearch",
-				m*n, resultado[2], "["+resultado[0]+","+resultado[1]+"]");
-		
 		//Para visualizar la matriz:
-		/*for (int i = 0; i < m; i++) {
+		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
 			System.out.print(matrix[i][j] + " ");
 			}
 			System.out.println(",");
-			}*/
+			}
+		/* PARA LINEARMATSEARCH El coste de los casos mejor y peor será siempre el mismo,
+		 * ya que el algoritmo tiene que recorrer siempre todo el array.
+		 * Será m*n .
+		 * 
+		 * Si la matriz es de 50x50 y el elemento no se encuentra en ella, 
+		 * hará 50 iteraciones.
+		 */
 		
-/* El coste de los casos mejor y peor será siempre el mismo,
- * ya que el algoritmo tiene que recorrer siempre todo el array.
- * Será m*n .
- * 
- * Si la matriz es de 50x50 y el elemento no se encuentra en ella, 
- * hará 50 iteraciones.
- */
+		int[] resultado2 = successfulMatSearch(matrix, elementoABuscar);
 		
+		int iterationSum=0;
+		int[] resultado3;
+
+		// PROMEDIO
+		System.out.println("\n********PROMEDIO********");
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				resultado3=successfulMatSearch(matrix,matrix[i][j]);
+				iterationSum+=resultado3[2];
+			}
+		}
+		System.out.printf("%s; m*n: %d ; iterationSum: %d ;%s\n",
+				"successfulMatSearchPromedio", m*n, iterationSum/((m*n)-1), "");
+		
+		//CASO MEJOR 
+		int elementoCasoMejor = matrix[0][0];
+		System.out.println("\n********CASO MEJOR********");
+		int[] resultadoCasoMejor = successfulMatSearch(matrix, elementoCasoMejor);
+		
+		//CASO PEOR: el elemento no está o está en la última posicion
+		int elementoCasoPeor1 = matrix[m-1][n-1];
+		System.out.println("\n********CASO PEOR********");
+		int[] resultadoCasoPeor = successfulMatSearch(matrix, elementoCasoPeor1);
+		
+		
+		
+
+		/* En el mejor caso, se detiene inmediatamente.
+		 * En el peor caso, recorre toda la matriz.
+		*/
+		
+		
+		/* Si que ha mejorado respecto al algoritmo anterior, ya que no tiene que recorrer toda 
+		 * la matriz
+		 */
+		 
 }
 }
